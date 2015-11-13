34c42f6e969176c56d8101d6d425037ff55ff1d5
cgrossko <christiangrosskopf@yahoo.com>
2015-11-13 03:04:31 +0000

    website

---
+++ js/colorbox.js
@@ -0,0 +1,43 @@
+
+
+//Problem: Images go to a dead end
+//Solution: Create an overlay with lightbox
+
+var $overlay = $('<div id="overlay"></div>');
+var $image = $("<img>");
+var $caption = $("<p></p>");
+
+// An image.
+$overlay.append($image);
+
+    // A caption.
+$overlay.append($caption);
+
+// Add overlay
+$("body").append($overlay);
+    
+
+
+// Capture the click event on a link to an image.
+$("#gallery a").click(function(event) {
+    event.preventDefault();
+    var imageLocation = $(this).attr("href");
+// Update overlay with the image linked in the link.
+    $image.attr("src", imageLocation);
+    
+    // Show the overlay.
+    $overlay.show();
+    
+        // Get child's alt attribute and set as caption.
+    var captionText = $(this).children("img").attr("alt");  
+    $caption.text(captionText);
+});
+
+// When overlay is clicked
+$overlay.click(function() {
+    // Hide the overlay.
+    $overlay.hide();
+});
+    
+
+
